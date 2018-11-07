b45b54d2bba793c1f1a12e6585404f668095d179
sklee <hadesleeq@gmail.com>
2018-11-07 17:07:03 +0900

    Board 클래스 생성

---
+++ src/main/java/com/tistory/hadeslee/springbootcommunityweb/domain/Board.java
@@ -0,0 +1,55 @@
+package com.tistory.hadeslee.springbootcommunityweb.domain;
+
+import com.tistory.hadeslee.springbootcommunityweb.domain.enums.BoardType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table
+public class Board implements Serializable {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Column
+    private String title;
+
+    @Column
+    private String subTitle;
+
+    @Column
+    private String content;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private BoardType boardType;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updateDate;
+
+    @OneToOne
+    private User user;
+
+    @Builder
+    public Board(String title, String subTitle, String content, BoardType boardType, LocalDateTime createdDate, LocalDateTime updateDate, User user) {
+        this.title = title;
+        this.subTitle = subTitle;
+        this.content = content;
+        this.boardType = boardType;
+        this.createdDate = createdDate;
+        this.updateDate = updateDate;
+        this.user = user;
+    }
+}
