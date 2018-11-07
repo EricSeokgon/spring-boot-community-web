19881410a109556233e23d09d6c7a12782389eb7
sklee <hadesleeq@gmail.com>
2018-11-07 17:01:31 +0900

    BoardType Enum 생성

--- src/main/java/com/tistory/hadeslee/springbootcommunityweb/domain/enums/BoardType.java
+++ src/main/java/com/tistory/hadeslee/springbootcommunityweb/domain/enums/BoardType.java
@@ -1,4 +1,17 @@
 package com.tistory.hadeslee.springbootcommunityweb.domain.enums;
 
-public class BoardType {
+public enum BoardType {
+
+    notice("공지사항"),
+    free("자유게시판");
+
+    private String value;
+
+    BoardType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
 }
