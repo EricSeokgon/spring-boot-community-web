030222b268758c49316d57fb87efd9a188578ba5
sklee <hadesleeq@gmail.com>
2018-11-07 17:58:45 +0900

    BoardRepository 인터페이스 생성

--- src/main/java/com/tistory/hadeslee/springbootcommunityweb/repository/BoardRepository.java
+++ src/main/java/com/tistory/hadeslee/springbootcommunityweb/repository/BoardRepository.java
@@ -4,6 +4,6 @@ import com.tistory.hadeslee.springbootcommunityweb.domain.Board;
 import com.tistory.hadeslee.springbootcommunityweb.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Board findByEmail(String email);
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    Board findByUser(User user);
 }
