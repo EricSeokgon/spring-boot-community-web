e31987c1e5b62386bd18787613c6af6c55504584
sklee <hadesleeq@gmail.com>
2018-11-09 15:48:26 +0900

    UserArgumentResolver 등록

--- src/main/java/com/tistory/hadeslee/springbootcommunityweb/SpringBootCommunityWebApplication.java
+++ src/main/java/com/tistory/hadeslee/springbootcommunityweb/SpringBootCommunityWebApplication.java
@@ -5,19 +5,32 @@ import com.tistory.hadeslee.springbootcommunityweb.domain.User;
 import com.tistory.hadeslee.springbootcommunityweb.domain.enums.BoardType;
 import com.tistory.hadeslee.springbootcommunityweb.repository.BoardRepository;
 import com.tistory.hadeslee.springbootcommunityweb.repository.UserRepository;
+import com.tistory.hadeslee.springbootcommunityweb.resolver.UserArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class SpringBootCommunityWebApplication {
+public class SpringBootCommunityWebApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCommunityWebApplication.class, args);
+    }
+
+    @Autowired
+    private UserArgumentResolver userArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(userArgumentResolver);
     }
 
     @Bean
