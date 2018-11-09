aafe4485d2c7ad8a235eae013cfee6be11d94651
sklee <hadesleeq@gmail.com>
2018-11-09 15:36:45 +0900

    UserInfotokenService 클래스 커스터마이징

--- src/main/java/com/tistory/hadeslee/springbootcommunityweb/config/SecurityConfig.java
+++ src/main/java/com/tistory/hadeslee/springbootcommunityweb/config/SecurityConfig.java
@@ -1,12 +1,95 @@
 package com.tistory.hadeslee.springbootcommunityweb.config;
 
+import com.tistory.hadeslee.springbootcommunityweb.domain.enums.SocialType;
 import com.tistory.hadeslee.springbootcommunityweb.oauth.ClientResources;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.CompositeFilter;
+
+import java.util.ArrayList;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+@EnableOAuth2Client
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private OAuth2ClientContext oAuth2ClientContext;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/login/**",  "/css/**", "/images/**", "/js/**", "/console/**").permitAll()
+                .antMatchers("/facebook").hasAuthority(FACEBOOK.getRoleType())
+                .antMatchers("/google").hasAuthority(GOOGLE.getRoleType())
+                .antMatchers("/kakao").hasAuthority(KAKAO.getRoleType())
+                .anyRequest().authenticated()
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+                .and()
+                .formLogin()
+                .successForwardUrl("/board/list")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .and()
+                .addFilterBefore(filter, CsrfFilter.class)
+                .addFilterBefore(oauth2Filter(), BasicAuthenticationFilter.class)
+                .csrf().disable();
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+    private Filter oauth2Filter() {
+        CompositeFilter filter = new CompositeFilter();
+        List<Filter> filters = new ArrayList<>();
+        filters.add(oauth2Filter(facebook(), "/login/facebook", FACEBOOK));
+        filters.add(oauth2Filter(google(), "/login/google", GOOGLE));
+        filters.add(oauth2Filter(kakao(), "/login/kakao", KAKAO));
+        filter.setFilters(filters);
+        return filter;
+    }
+
+    private Filter oauth2Filter(ClientResources client, String path, SocialType socialType) {
+        OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
+        OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oAuth2ClientContext);
+
+        filter.setRestTemplate(template);
+        filter.setTokenServices(new UserTokenService(client, socialType));
+        filter.setAuthenticationSuccessHandler((request, response, authentication) -> response.sendRedirect("/" + socialType.getValue() + "/complete"));
+        filter.setAuthenticationFailureHandler((request, response, exception) -> response.sendRedirect("/error"));
+        return filter;
+    }
+
     @Bean
     @ConfigurationProperties("facebook")
     public ClientResources facebook() {
@@ -24,5 +107,4 @@ public class SecurityConfig {
     public ClientResources kakao() {
         return new ClientResources();
     }
-
 }
